85ce332512577513c613a9b709f5b61fae84d408
graemerenfrew <graemerenfrew+github@gmail.com>
2018-08-06 22:30:22 +0200

    templates and inheritance

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -3,7 +3,7 @@ from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models import Q
 
 GAME_STATUS_CHOICES = {
     ('F', 'First Player to Move'),
@@ -12,6 +12,21 @@ GAME_STATUS_CHOICES = {
     ('L', 'Second Player Wins'),
     ('D', 'Draw'),
 }
+
+class GameQuerySet(models.QuerySet):
+    ''' this will represent a collection of objects from the database'''
+    def games_for_user(self, user):
+
+        #We have imported Q which lets us do queries with logical OR in them
+        return self.filter(
+            Q(first_player=user) | Q(second_player=user)
+        )
+
+    def active(self):
+        ''' return active games'''
+        return self.filter(
+            Q(status='F') | Q(status='S')
+        )
 
 @python_2_unicode_compatible
 class Game(models.Model):
@@ -25,6 +40,10 @@ class Game(models.Model):
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
+
+
+    #Create an overwritten manager object that will contain the objects from the GamesQuerySet custom object
+    objects = GameQuerySet.as_manager()
 
     def __str__(self):
         return "{0} vs {1}".format(self.first_player, self.second_player)
--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-
+from gameplay.models import Game
 # Create your views here.
 
 
 def home(request):
-    ''' Lets delegate the creation of the html to the template using the render function'''
-    return render(request, "player/home.html")
+    '''
+
+        Lets delegate the creation of the html to the template using the render function
+        Get the list of games that this current player is involved in
+
+    '''
+    '''
+    games_first_player = Game.objects.filter(
+        first_player=request.user,
+        status='F'
+    )
+    games_second_player = Game.objects.filter(
+        second_player=request.user,
+        status='S'
+    )
+    all_my_games = list(games_first_player) + \
+                    list(games_second_player)
+    '''
+    my_games = Game.objects.games_for_user(request.user)
+    active_games = my_games.active()
+
+    return render(request, "player/home.html",
+                  {'games': active_games})
