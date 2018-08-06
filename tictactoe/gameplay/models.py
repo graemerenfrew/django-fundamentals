0389db9dfa8de1577247c8ea2bc8e73e520edd93
graemerenfrew <graemerenfrew+github@gmail.com>
2018-08-06 18:51:28 +0200

    CASCADE fiddling

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
 
 class Game(models.Model):
     first_player = models.ForeignKey(User,
@@ -10,12 +12,16 @@ class Game(models.Model):
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
 
+
 class Move(models.Model):
     ''' this is a model class and will rep a table in the database
         PrimaryKey will be added automatically
     '''
     x = models.IntegerField()
     y = models.IntegerField()
-    comment = models.CharField(max_length=300, blank=True) #We can have an empty comment in a CharField
+    comment = models.CharField(max_length=300, blank=True)
     by_first_player = models.BooleanField()
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
 
