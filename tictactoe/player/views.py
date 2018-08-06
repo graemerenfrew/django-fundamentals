a3fe0f2c43627b5db70543579103dabee228a8d3
graemerenfrew <graemerenfrew+github@gmail.com>
2018-08-06 21:28:41 +0200

    home page for player app

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    ''' Lets delegate the creation of the html to the template using the render function'''
+    return render(request, "player/home.html")
