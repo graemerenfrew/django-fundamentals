b89c2d02ce63879e2d6e6741f2f23ba5e5cf55e4
graemerenfrew <graemerenfrew+github@gmail.com>
2018-08-06 20:45:01 +0200

    string for the game model

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -13,6 +15,9 @@ class Game(models.Model):
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='F')
+
+    def __str__(self):
+        return "{0} vs {1}".format(self.first_player, self.second_player)
 
 
 class Move(models.Model):
