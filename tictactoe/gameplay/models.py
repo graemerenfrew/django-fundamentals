d05f7847edba7abf962dd622ab0d4d5cd4aa63ac
graemerenfrew <graemerenfrew+github@gmail.com>
2018-08-06 20:54:18 +0200

    admin site customisation

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,9 +1,19 @@
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 from django.contrib.auth.models import User
 
 
+GAME_STATUS_CHOICES = {
+    ('F', 'First Player to Move'),
+    ('S', 'Second Player to Move'),
+    ('W', 'First Player Wins'),
+    ('L', 'Second Player Wins'),
+    ('D', 'Draw'),
+}
+
+@python_2_unicode_compatible
 class Game(models.Model):
     first_player = models.ForeignKey(User,
                                      related_name="games_first_player",
@@ -14,7 +24,7 @@ class Game(models.Model):
 
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, default='F')
+    status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
 
     def __str__(self):
         return "{0} vs {1}".format(self.first_player, self.second_player)
