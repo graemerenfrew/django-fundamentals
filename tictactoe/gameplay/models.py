019a4f38ac8283175846a5981de42d9a8fb3b9fd
graemerenfrew <graemerenfrew+github@gmail.com>
2018-08-06 18:31:56 +0200

    create the models

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    first_player = models.ForeignKey(User,
+                                     related_name="games_first_player")
+    second_player = models.ForeignKey(User,
+                                      related_name="games_second_player")
+    start_time = models.DateTimeField(auto_now_add=True)
+    last_active = models.DateTimeField(auto_now=True)
+
+class Move(models.Model):
+    ''' this is a model class and will rep a table in the database
+        PrimaryKey will be added automatically
+    '''
+    x = models.IntegerField()
+    y = models.IntegerField()
+    comment = models.CharField(max_length=300, blank=True) #We can have an empty comment in a CharField
+    by_first_player = models.BooleanField()
+
