562d2dbe7dcf5d4739bba48947da4dadd5508635
graemerenfrew <graemerenfrew+github@gmail.com>
2018-08-07 12:50:03 +0200

    implemented crispy forms

--- tictactoe/player/models.py
+++ tictactoe/player/models.py
@@ -3,12 +3,17 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Invitation(models.Model):
     from_user = models.ForeignKey(User,
-                                  related_name="invitations_send",
+                                  related_name="invitations_sent",
                                   on_delete=models.CASCADE)
     to_user = models.ForeignKey(User,
                                 related_name="invitations_received",
+                                verbose_name="User to invite",
+                                help_text="Please select the user you wnat to challenge!",
                                 on_delete=models.CASCADE)
-    message = models.CharField(max_length=300)
+    message = models.CharField(max_length=300, blank=True,
+                               verbose_name="Optional message",
+                               help_text="Always nice to be friendly!")
     timestamp = models.DateTimeField(auto_now_add=True)
